2324ce7ea7cd5b6cdb96fdb7f9303e5087f5d7d9
hungrated <zhang295415658@qq.com>
2018-04-14 22:26:56 +0800

    master: fix bugs

--- src/hello.py
+++ src/hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return '<h1>Hello Python Web!</h1><br><br><a href="127.0.0.1:9090/signin">Sign In</a>'
+    return '<h1>Hello Python Web!</h1><br><br><a href="http://127.0.0.1:9090/signin">Sign In</a>'
 
 
 @app.route('/signin', methods=['GET'])
@@ -15,14 +15,14 @@ def signin_form():
               <p><input name="username"></p>
               <p><input name="password" type="password"></p>
               <p><button type="submit">Sign In</button></p>
-              </form><br><a href="127.0.0.1:9090/">Back To Home</a>'''
+              </form><br><a href="http://127.0.0.1:9090/">Back To Home</a>'''
 
 
 @app.route('/signin', methods=['POST'])
 def signin():
     if request.form['username'] == 'admin' and request.form['password'] == 'admin':
-        return '<h3>Hello, admin!</h3><br><a href="127.0.0.1:9090/">Back To Home</a>'
-    return '<h3>Bad username or password.</h3><br><a href="127.0.0.1:9090/">Back To Home</a>'
+        return '<h3>Hello, admin!</h3><br><a href="http://127.0.0.1:9090/">Back To Home</a>'
+    return '<h3>Bad username or password.</h3><br><a href="http://127.0.0.1:9090/">Back To Home</a>'
 
 
 if __name__ == '__main__':
