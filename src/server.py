f5622a6266add088fc3d157e54c101f51604a7af
hungrated <zhang295415658@qq.com>
2018-04-14 22:08:36 +0800

    master: add simple server

--- src/server.py
+++ src/server.py
@@ -1,3 +1,8 @@
 from wsgiref.simple_server import make_server
 
 from hello import application
+
+httpd = make_server('', 9090, application)
+
+print('Serving HTTP on port 9090...')
+print('http://127.0.0.1:9090')
