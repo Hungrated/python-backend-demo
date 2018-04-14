7aa3c48fb20c1293649da0fe71281ec3ddb87123
hungrated <zhang295415658@qq.com>
2018-04-14 22:11:30 +0800

    master: fix bugs

--- src/hello.py
+++ src/hello.py
@@ -1,3 +1,3 @@
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'test/html')])
-    return ['<h1>Hello Python Web!</h1>']
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return [b'<h1>Hello Python Web!</h1>']
--- src/server.py
+++ src/server.py
@@ -6,3 +6,5 @@ httpd = make_server('', 9090, application)
 
 print('Serving HTTP on port 9090...')
 print('http://127.0.0.1:9090')
+
+httpd.serve_forever()
